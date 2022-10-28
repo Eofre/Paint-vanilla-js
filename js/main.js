421670fa81dbbde16d36ec380db7047edd8f34cf
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-29 01:09:00 +0800

    add events

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const canvas = document.querySelector("#jsCanvas");
+const context = canvas.getContext("2d");
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(e) {
+  let x = e.offsetX;
+  let y = e.offsetY;
+}
+
+function onMouseDown(e) {
+  painting = true;
+}
+
+function onMouseUp(e) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
